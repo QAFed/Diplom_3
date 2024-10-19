e8c181f453b202bf87ae62f8acb7a40c0558bfe5
QAFed <143259597+QAFed@users.noreply.github.com>
2024-10-19 21:11:31 +0300

    stopeeee - relax

--- tests/test_login_user_events.py
+++ tests/test_login_user_events.py
@@ -1,13 +1,23 @@
 from pages.home_page import HomePage
 from pages.header_page import HeaderElements
 from pages.personal_page import PersonalPage
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 class TestLogiUserEvents:
     def test_open_personal_page_from_link_klick_on_home_page(self, login_user):
-        home_page = HomePage(login_user)
-        home_page.click_element(HeaderElements.button_personal_account)
-        home_page.wait_element(PersonalPage.button_order_history)
+        action_page = HomePage(login_user)
+        action_page.click_element(HeaderElements.button_personal_account)
+        action_page.wait_element(PersonalPage.button_order_history)
+        assert login_user.current_url == PersonalPage.URL
+
+    def test_open_history_page_from_button_on_personal_page(self, login_user):
+        action_page = HomePage(login_user)
+        action_page.click_element(HeaderElements.button_personal_account)
+        action_page.wait_element(PersonalPage.button_order_history)
+        action_page.click_element(PersonalPage.button_order_history)
+        WebDriverWait(login_user, 5).until(expected_conditions.url_to_be(expected_url)
+        )
         assert login_user.current_url == PersonalPage.URL
 
 
