e60b40206c8e3df5ddd7768052a84bb882976803
QAFed <143259597+QAFed@users.noreply.github.com>
2024-10-20 13:00:01 +0300

    dinner

--- tests/test_main_functions.py
+++ tests/test_main_functions.py
@@ -5,22 +5,22 @@ from pages.ingredient_detail import IngrdientDetailsPage
 
 class TestMainFunctions:
     def test_open_page_constructor_from_button_in_header(self, driver_factory):
-        current_page = OrderListPage(driver_factory)
-        current_page.open_page()
-        current_page.ac_click_element(HeaderElements.button_constructor)
-        current_page.wait_element(HomePage.header_text)
+        action_page = OrderListPage(driver_factory)
+        action_page.open_page()
+        action_page.ac_click_element(HeaderElements.button_constructor)
+        action_page.wait_element(HomePage.header_text)
         assert driver_factory.current_url == HomePage.URL
 
     def test_open_page_order_list_from_button_in_header(self, driver_factory):
-        current_page = HomePage(driver_factory)
-        current_page.open_page()
-        current_page.ac_click_element(HeaderElements.button_order_list)
-        current_page.wait_element(OrderListPage.header_text)
+        action_page = HomePage(driver_factory)
+        action_page.open_page()
+        action_page.ac_click_element(HeaderElements.button_order_list)
+        action_page.wait_element(OrderListPage.header_text)
         assert driver_factory.current_url == OrderListPage.URL
 
     def test_open_page_inredient_details_if_click_icon_on_home_page(self, driver_factory):
-        current_page = HomePage(driver_factory)
-        current_page.open_page()
-        current_page.ac_click_element(HomePage.icon_krator_bulka)
-        current_page.wait_element(IngrdientDetailsPage.header_text)
+        action_page = HomePage(driver_factory)
+        action_page.open_page()
+        action_page.ac_click_element(HomePage.icon_krator_bulka)
+        action_page.wait_element(IngrdientDetailsPage.header_text)
         assert IngrdientDetailsPage.URLpart in driver_factory.current_url
